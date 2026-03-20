29bfc8cb9b032028ebd4227bfcdcef7cdd0c505d
Gagan Suman <fbdgagansuman@gmail.com>
2026-03-20 11:00:01 +0530

    commit

--- auth/backend/db/dbConnect.js
+++ auth/backend/db/dbConnect.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 const connectDB = async() => {
     // await mongoose.connect(process.env.MONGO_URI || "mongodb://Gagan:gagan@cluster0.fkloojo.mongodb.net/?appName=Cluster0", {}).then(() => {
